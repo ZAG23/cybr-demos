8ac8c4d16be191d400e2523403b9c1f33dae55de
david.lang <david.lang@cyberark.com>
2026-01-12 15:55:59 -0800

    update

--- compute_init/windows/setup.ps1
+++ compute_init/windows/setup.ps1
@@ -1,28 +1,27 @@
+Write-Host "# Running Setup"
+
+$ScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
 #--------------------------------------------------------------------------------------------------
 New-Item -Path "C:\init_started.txt" -ItemType File | Out-Null
 #--------------------------------------------------------------------------------------------------
 Start-Transcript -Append C:\init_log.txt
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 #--------------------------------------------------------------------------------------------------
-
-#& ".\install_nuget.ps1"
-& ".\install_git.ps1"
-& ".\install_ps7.ps1"
-& ".\install_notepad++.ps1"
-#& ".\install_ssh.ps1"
-& ".\install_aws_tools.ps1"
-& ".\install_chrome.ps1"
-& ".\install_iis.ps1"
-
+#& "$ScriptRoot\install_nuget.ps1"
+& "$ScriptRoot\install_git.ps1"
+& "$ScriptRoot\install_ps7.ps1"
+& "$ScriptRoot\install_notepad++.ps1"
+#& "$ScriptRoot\install_ssh.ps1"
+& "$ScriptRoot\install_aws_tools.ps1"
+& "$ScriptRoot\install_chrome.ps1"
+& "$ScriptRoot\install_iis.ps1"
 #--------------------------------------------------------------------------------------------------
 
-
 #--------------------------------------------------------------------------------------------------
-
-$ScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
 Import-Module "$ScriptRoot\module_pin_to_taskbar.psm1" -Force
 
 Write-Host "# Setting up Pins for Apps on Taskbar"
+
 [PinToTaskBar_Verb] $pin = [PinToTaskBar_Verb]::new();
 
 $pin.Pin("C:\Windows\notepad.exe")
@@ -34,10 +33,9 @@ $pin.Pin("C:\Windows\System32\inetsrv\InetMgr.exe")
 $pin.Pin("C:\Windows\system32\compmgmt.msc")
 $pin.Pin("C:\Windows\system32\services.msc")
 $pin.Pin("C:\Windows\system32\taskschd.msc")
-
 #--------------------------------------------------------------------------------------------------
 New-Item -Path "C:\init_completed.txt" -ItemType File | Out-Null
 #--------------------------------------------------------------------------------------------------
 Stop-Transcript
-Restart-Computer -ComputerName localhost -Force
+#Restart-Computer -ComputerName localhost -Force
 #--------------------------------------------------------------------------------------------------
