f3c4b1eba7b0a017f362fd51565885fa3ef93070
david.lang <david.lang@cyberark.com>
2026-01-12 16:15:38 -0800

    update

--- compute_init/windows/install_notepad++.ps1
+++ compute_init/windows/install_notepad++.ps1
@@ -1,5 +1,5 @@
 Write-Host "# Installing NotePad++"
 
-Invoke-WebRequest "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.5/npp.8.4.5.Installer.exe" -OutFile "c:\\bootstrap\tmp\npp.Installer.exe"
+Invoke-WebRequest "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.8.8/npp.8.8.8.Installer.exe" -OutFile "c:\\bootstrap\tmp\npp.Installer.exe"
 Start-Process "c:\\bootstrap\tmp\npp.Installer.exe" /S -NoNewWindow -Wait -PassThru
 start-sleep -s 2
