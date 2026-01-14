9356af7bef38069af25885bb1880f3cf5ec849e0
david.lang <david.lang@cyberark.com>
2026-01-13 18:09:42 -0800

    update

--- demos/credential_providers/agent_windows/setup/install_cp.ps1
+++ demos/credential_providers/agent_windows/setup/install_cp.ps1
@@ -1,33 +1,42 @@
 Start-Transcript -Append C:\PSScriptLog.txt
 Get-Date -UFormat "%A /%Y%m/%d %R %Z"
-Set-ExecutionPolicy Bypass -Scope process
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
-$CYBR_DEMOS_PATH = "$HOME\cybr-demos"
+$ScriptRoot      = Split-Path -Parent $MyInvocation.MyCommand.Path
+$CYBR_DEMOS_PATH = "C:\cybr-demos"
 
 # Load utility functions
-#demo_path
 . "$CYBR_DEMOS_PATH\demos\utility\powershell5\dotenv_functions.ps1"
 . "$CYBR_DEMOS_PATH\demos\utility\powershell5\aws_functions.ps1"
 . "$CYBR_DEMOS_PATH\demos\tenant_vars.ps1"
 
 # Local setup
-Load-DotEnv "vars.env"
-$s3_uri = $S3_URI
-$zip_file = $ZIP_FILE
-$s3_akey = $S3_AKEY
-$s3_skey = $S3_SKEY
-$vault_fqdn = "vault-$TENANT_SUBDOMAIN.privilegecloud.cyberark.cloud"
+Load-DotEnv "$ScriptRoot\vars.env"
 
-###
-### Add Agent IP to Privledge Cloud Allow List
-###
+$s3_uri    = $S3_URI
+$zip_file  = $ZIP_FILE
+if (-not $s3_uri)   { throw "S3_URI is empty after Load-DotEnv" }
+if (-not $zip_file) { throw "ZIP_FILE is empty after Load-DotEnv" }
 
-# Get File from S3
-Connect-AWS -AccessKey $s3_akey -SecretKey $s3_skey
+$vault_fqdn = "vault-${TENANT_SUBDOMAIN}.privilegecloud.cyberark.cloud"
+
+# Get file from S3
+$region = $env:AWS_REGION
+if (-not $region) { $region = "us-east-1" }
+
+if ($env:AWS_ACCESS_KEY_ID -and $env:AWS_SECRET_ACCESS_KEY) {
+    Connect-AWS -AccessKey $env:AWS_ACCESS_KEY_ID -SecretKey $env:AWS_SECRET_ACCESS_KEY -Region $region
+} else {
+    Write-Host "No AWS keys found using IAM Role"
+    Initialize-AWSDefaultConfiguration -Region $region
+}
+
 Get-S3File $s3_uri
 
-Expand-Archive -Path $zip_file -DestinationPath ./installer -Force
-cd "./installer"
+# Expand installer
+$installerDir = Join-Path $ScriptRoot "installer"
+Expand-Archive -Path $zip_file -DestinationPath $installerDir -Force
+Set-Location $installerDir
 
 # Steps to Create the silent.ini file in Record mode
 # !Do not use cred file, advanced setup, vault IP, cred user name
@@ -37,38 +46,32 @@ cd "./installer"
 # and saves them to a response file.
 # The response file is fully defined and can be used when installing other CPs using Full silent mode.
 # This is especially useful if you are setting up a wide-scale installation of CPs.
-# $record_file = "$(pwd)\record.iss"
+# $record_file = Join-Path $PWD "record.iss"
 # .\setup.exe /r /f1"$record_file"
 
+# Prepare silent response file
+$record_file = Join-Path $CYBR_DEMOS_PATH "demos\credential_providers\agent_windows\setup\record.iss"
+$silent_file = Join-Path $PWD "silent.iss"
 
-$record_file = "record.iss"
-$silent_file = "silent.iss"
-cp ../$record_file $silent_file
-(Get-Content $silent_file) -replace "szEdit1='\s*'", ("szEdit1='$vault_fqdn'") | Set-Content $silent_file
+Copy-Item -Path $record_file -Destination $silent_file -Force
 
-start-sleep -s 1
-## first run partialy succededs?
-.\setup.exe /s /f1"silent.iss"
-start-sleep -s 2
+# Read the whole file
+$content = Get-Content -Path $silent_file -Raw
 
-#
-## edit vault ini file
-#$vault_path = "C:\Program Files\CyberArk\ApplicationPasswordProvider\Vault\Vault.ini"
-##rm $vault_path
-#$content = [System.IO.File]::ReadAllText("$vault_path").Replace("{{vault_fqdn}}",$vault_ip)
-#[System.IO.File]::WriteAllText("$vault_path", $content)
-#
-#
-## Run script after reboot
-#$scriptPath = "C:\Cyberark\cp\after_reboot.ps1"
-#$taskName = "after_reboot_cp_install"
-#$taskpath = "\cybr-demos"
-#$argument = "-WindowStyle Hidden -Command `"& '$scriptPath'`""
-#$action = (New-ScheduledTaskAction -Execute "${Env:WinDir}\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument $argument)
-#$trigger = (New-ScheduledTaskTrigger -AtStartup)
-#$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-#
-#Register-ScheduledTask -TaskName "$taskName" -TaskPath $taskPath -Action $action -Trigger $trigger -Principal $principal ;
+$apos = [char]39
+$pattern = "szEdit1=$apos[^$apos]*$apos"
+$replacement = "szEdit1=$apos$vault_fqdn$apos"
+
+$content = [regex]::Replace($content, $pattern, $replacement)
+
+# Write it back
+Set-Content -Path $silent_file -Value $content
+
+Start-Sleep -Seconds 1
+
+& ".\setup.exe" "/s" "/f1$silent_file" "$env:INSTALLER_USR;$env:INSTALLER_PWD"
+
+Start-Sleep -Seconds 2
 
 Get-Date -UFormat "%A /%Y%m/%d %R %Z"
 Stop-Transcript
---
+++ demos/credential_providers/agent_windows/setup/record.iss
@@ -0,0 +1,34 @@
+[InstallShield Silent]
+Version=v7.00
+File=Response File
+[File Transfer]
+OverwrittenReadOnly=NoToAll
+[{718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-DlgOrder]
+Dlg0={718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdWelcome-0
+Count=5
+Dlg1={718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdSetupType-0
+Dlg2={718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdRegisterUser-0
+Dlg3={718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdSetupType2-0
+Dlg4={718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdShowDlgEdit2-0
+[{718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdWelcome-0]
+Result=1
+[{718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdSetupType-0]
+szDir=' '
+Result=302
+[{718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdRegisterUser-0]
+szName=WizardUser
+szCompany=WizardCompany
+Result=1
+[{718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdSetupType2-0]
+szDir=C:\Program Files\CyberArk
+Result=304
+[Application]
+Name=CyberArk Application Access Manager
+Version=14.2.4.3
+Company=CyberArk
+Lang=0409
+[{718BA81E-A95C-4112-86A1-40F0FE8BDE9E}-SdShowDlgEdit2-0]
+szEdit1=vault-WizardTenantPreFix.privilegecloud.cyberark.cloud
+szEdit2=1858
+szProviderUser"CCP_CP_User"
+Result=1
